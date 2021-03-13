62aeb008596c41cf39add7d6f1fa739ccd724cee
EmilyJie <emily6331@gmail.com>
2021-03-13 23:22:09 +0800

    wk3-hw finished

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+export default function NavBar() {
+    return (
+        <ul className="navbar bg-color list-unstyled">
+            <li className="navitem">Tableware</li>
+            <li className="navitem">Cookware</li>
+            <li className="navitem">Home accessories</li>
+            <li className="navitem">Lighting</li>
+            <li className="navitem">Textile</li>
+            <li className="navitem">Furniture</li>
+        </ul>
+    )
+}
